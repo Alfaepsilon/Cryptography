d9a88b288edaf86c104a41b9596588a856de5d62
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2025-11-10 18:11:03 +0100

    Update Assymetric.h

--- Assymetric/Assymetric.h
+++ Assymetric/Assymetric.h
@@ -1,7 +1,1 @@
-int primitive_roots();
-unsigned long long exeucalgo();
-int gcd();
-int shift_cipher();
-unsigned long long fastpoweralgo_lowmem();
-unsigned long long DLP();
 unsigned long long shanks();
