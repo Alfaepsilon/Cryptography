36d61e9bbe329ad35ede4cd0e2492633b5f4da96
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2025-11-17 20:25:45 +0100

    Update and rename gcd.c to cc_gcd.c

---
+++ Assymetric/Miscellaneous/cc_gcd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+
+mpz_t* cc_gcd(mpz_t N, mpz_t a) {
+	mpz_t A, b;
+	mpz_init_set(A, N);
+	mpz_init_set(b, a);
+	while (mpz_cmp(A, b) != 0) {
+		if (mpz_cmp(A, b) > 0) {
+			mpz_sub(A, A, b);
+		}
+		else {
+			mpz_sub(b, b, A);
+		}
+	}
+	mpz_t* gcd = malloc(sizeof(mpz_t));
+	mpz_init_set(*gcd, A);
+	mpz_clears(A, b, (mpz_ptr)NULL);
+	return gcd;
+}
